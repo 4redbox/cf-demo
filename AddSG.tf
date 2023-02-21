f902605a5c440d132be243c48cc912aaa33b0f8a
Saravanesh <raja4redbox@gmail.com>
2023-02-21 07:38:01 +0530

    Create AddSG.tf

---
+++ AddSG.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_security_group" "example" {
+  name_prefix = "example-sg"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  security_groups = [aws_security_group.example.id]
+
+  tags = {
+    Name = "example-instance"
+  }
+}
